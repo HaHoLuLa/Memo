f149eea0a6114eae5772d4aa22e603fd00b65caf
HaHoLuLa <jh610.lee@gmail.com>
2025-01-29 15:33:20 +0900

    기본 세팅 완료

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log('개발');
+    mainWindow.loadURL('http://localhost:3000');
+  } else {
+    console.log('배포');
+    mainWindow.loadFile(path.join(__dirname, './renderer/dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
